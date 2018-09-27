e0cc12e0b1c1060791faff6391f68bc31f11977a
Alex Scott <codetheorist@gmail.com>
2018-09-27 20:07:28 +0100

    Update homepage content

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,18 @@
     <div>
       <logo/>
       <h1 class="title">
-        ameliajanekelleher
+        Amelia Jane Kelleher's
       </h1>
       <h2 class="subtitle">
-        My grand Nuxt.js project
+        Personal Web Site
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <nuxt-link 
+          to="/" 
+          class="button--green">Home</nuxt-link>
+        <nuxt-link 
+          to="about" 
+          class="button--grey">About</nuxt-link>
       </div>
     </div>
   </section>
@@ -33,7 +31,6 @@ export default {
 </script>
 
 <style>
-
 .container {
   min-height: 100vh;
   display: flex;
